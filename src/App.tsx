29b469b4edfe4b2d0577858bc37a941751cf4b29
JulieHM <julieholtemotland@hotmail.com>
2021-02-09 14:26:42 +0100

    first steps setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// import firebase from "firebase/app";
+
+// import "firebase/auth";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC3VrrCu2PeD4nQMDSCHsaZQ1rhUE7U03Y",
+//   authDomain: "arrangere-a8fca.firebaseapp.com",
+//   projectId: "arrangere-a8fca",
+//   storageBucket: "arrangere-a8fca.appspot.com",
+//   messagingSenderId: "453649241479",
+//   appId: "1:453649241479:web:845ba0b1ad3d50b0f2af22",
+//   measurementId: "G-CVE4Q4HSNK",
+// };
+
+// firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
