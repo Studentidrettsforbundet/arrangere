ffcb126fa939b9fb6fdb09a6e4692370e6db47c8
emilielr <43408175+emilielr@users.noreply.github.com>
2021-02-09 15:22:53 +0100

    #2 Connect Firebase to our project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from "react";
 import "./App.css";
 
-// import firebase from "firebase/app";
+import firebase from "firebase/app";
 
-// import "firebase/auth";
-// import "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC3VrrCu2PeD4nQMDSCHsaZQ1rhUE7U03Y",
-//   authDomain: "arrangere-a8fca.firebaseapp.com",
-//   projectId: "arrangere-a8fca",
-//   storageBucket: "arrangere-a8fca.appspot.com",
-//   messagingSenderId: "453649241479",
-//   appId: "1:453649241479:web:845ba0b1ad3d50b0f2af22",
-//   measurementId: "G-CVE4Q4HSNK",
-// };
-
-// firebase.initializeApp(firebaseConfig);
+import "firebase/auth";
+import "firebase/firestore";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <p>Her skal det komme en søknadsside</p>
     </div>
   );
 }
