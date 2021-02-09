3569248461d9ba0f62287f83c734d699bb7e6e8c
emilielr <43408175+emilielr@users.noreply.github.com>
2021-02-09 15:25:06 +0100

    #2 Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./App.css";
 
-import firebase from "firebase/app";
-
-import "firebase/auth";
-import "firebase/firestore";
-
 function App() {
   return (
     <div className="App">
