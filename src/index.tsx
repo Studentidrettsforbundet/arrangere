a99edb80f8d0e3750cdfdef767ad15573cdae49c
emilielr <43408175+emilielr@users.noreply.github.com>
2021-02-11 09:08:10 +0100

    Place firebase config in right file

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC3VrrCu2PeD4nQMDSCHsaZQ1rhUE7U03Y",
+  authDomain: "arrangere-a8fca.firebaseapp.com",
+  databaseURL:
+    "https://arrangere-a8fca-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "arrangere-a8fca",
+  storageBucket: "arrangere-a8fca.appspot.com",
+  messagingSenderId: "453649241479",
+  appId: "1:453649241479:web:6450f5d0d16186e1f2af22",
+  measurementId: "G-5ZH2E86G8G",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
