b645fb6667ba98587e4ceef20dbd664eb2e1a1c7
johannasofieslinde <johannaslinde@icloud.com>
2021-02-15 14:16:38 +0100

    #7 Add 'studentidrett'-logo to drawer

--- src/components/DrawerBar.tsx
+++ src/components/DrawerBar.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Collapse from '@material-ui/core/Collapse';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
-import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
-import AppsOutlinedIcon from '@material-ui/icons/AppsOutlined';
-import { Divider } from '@material-ui/core';
+import React from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import DescriptionOutlinedIcon from "@material-ui/icons/DescriptionOutlined";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import AppsOutlinedIcon from "@material-ui/icons/AppsOutlined";
+import { Divider } from "@material-ui/core";
+import StudentidrettLogo from "./../images/studentidrett-logo-sort.png";
+import { CardMedia } from "@material-ui/core";
 
-const drawerWidth = 240;
+const drawerWidth = 300;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: '100%',
+      width: "100%",
       maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
     },
@@ -35,20 +33,24 @@ const useStyles = makeStyles((theme: Theme) =>
       width: drawerWidth,
     },
     nested: {
-        paddingLeft: theme.spacing(4),
-      },
+      paddingLeft: theme.spacing(4),
+    },
     content: {
       flexGrow: 1,
       backgroundColor: theme.palette.background.default,
       padding: theme.spacing(3),
     },
-  }),
+    media: {
+      height: 22,
+      margin: 10,
+    },
+  })
 );
 
 export default function DrawerBar() {
   const classes = useStyles();
 
-  const [open, setOpen] = React.useState(true); {/* Sjekk state håndtering, bruke recoil her? */}
+  const [open, setOpen] = React.useState(true);
 
   const handleClick = () => {
     setOpen(!open);
@@ -57,58 +59,53 @@ export default function DrawerBar() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-          <div className="drawerContent"> {/* Sjekk om dette er riktig måte å skrive klassenavn på */}
-
-          </div>
-        
-       <List
-      component="nav"
-     aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Meny
-        </ListSubheader>
-      }
-      className={classes.root}
-    >
-      <ListItem button>
-        <ListItemIcon>
-          <AppsOutlinedIcon />
-        </ListItemIcon>
-        <ListItemText primary="Hjem" />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon> 
-          <PersonOutlineOutlinedIcon />
-        </ListItemIcon>
-        <ListItemText primary="Brukerprofil" />
-      </ListItem>
-      <Divider/>
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <DescriptionOutlinedIcon />
-        </ListItemIcon>
-        <ListItemText primary="Mine søknader" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem button className={classes.nested}>
-            <ListItemText primary="Eksempel søknad 1" />
-          </ListItem>
-        </List>
-      </Collapse>
-    </List>
-      </Drawer>
-  
+      <div className="drawerContent">
+        {" "}
+        {/* Sjekk om dette er riktig måte å skrive klassenavn på */}
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            className={classes.root}
+          >
+            <CardMedia className={classes.media} image={StudentidrettLogo} />
+            <ListItem button>
+              <ListItemIcon>
+                <AppsOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Hjem" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <PersonOutlineOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Brukerprofil" />
+            </ListItem>
+            <Divider />
+            <ListItem button onClick={handleClick}>
+              <ListItemIcon>
+                <DescriptionOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Mine søknader" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem button className={classes.nested}>
+                  <ListItemText primary="Eksempel søknad 1" />
+                </ListItem>
+              </List>
+            </Collapse>
+          </List>
+        </Drawer>
+      </div>
     </div>
   );
 }
