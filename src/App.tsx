5f86a2fd0f031da7883c0556159158b26982fc38
JulieHM <julieholtemotland@hotmail.com>
2021-02-15 12:22:39 +0100

    Complete user interface for choose application form page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import ChooseApplicationForm from "./Components/ChooseApplicationForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <ChooseApplicationForm></ChooseApplicationForm>
       </header>
     </div>
   );
