9885a9d143569e3f8f8dcc224a83e6bdee3325a5
Siw Døvle <siwd@Siws-MacBook-Pro.local>
2021-02-16 13:35:32 +0100

    #17 Fix import issues

---
+++ src/test/securityRules.test.ts
@@ -0,0 +1,19 @@
+import * as assert from "assert";
+import * as firebase from "@firebase/testing";
+
+describe("Our app", () => {
+  it("Math", () => {
+    assert.equal(2 + 2, 4);
+  });
+
+  it("Can read items in the read-only collection", async () => {
+    const db = firebase
+      .initializeTestApp({
+        projectId: process.env.NODE_ENV_FIREBASE_PROJECT_ID
+      })
+      .firestore();
+    const testDoc = db.collection("readonly").doc("testDoc");
+    await firebase.assertSucceeds(testDoc.get());
+  });
+});
+
