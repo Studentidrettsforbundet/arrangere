230f57f2599c6ceb66310c011548779977255bf8
Sigrun N <sigrunn@Sigruns-MacBook-Pro.local>
2021-02-19 13:01:02 +0100

    #3 if user is null, don't convert to JSON

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,68 +1,86 @@
-import React, {useEffect, useRef,useState} from "react";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel'
-import {
-    useSetRecoilState,
-  } from 'recoil';
-  import { auth } from "../firebase";
-  import {currentUserState} from "../stateManagement/userAuth"
+import React, { useEffect, useRef, useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import { useRecoilState } from "recoil";
+import { auth } from "../firebase";
+import { currentUserState } from "../stateManagement/userAuth";
 
+const Login = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-const Login = () =>{
-   const emailRef = useRef<HTMLInputElement>(null);
-   const passwordRef = useRef<HTMLInputElement>(null);
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>("");
 
-   const setCurrentUser = useSetRecoilState(currentUserState)
-   const [loading, setLoading] = useState(false)
-   const [error, setError] = useState<string>("")
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user: any) => {
+      if (user != null) {
+        setCurrentUser(user.toJSON());
+      } else {
+        setCurrentUser(null);
+      }
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
-
-   useEffect(() =>{
-       const unsubscribe = auth.onAuthStateChanged( ( user :  any ) =>{
-           setCurrentUser(user.toJSON())
-           setLoading(false)
-   })
-   return unsubscribe
-    },[])
-
-    async function handleSubmit(e:any){
-        try{
-            setError("")
-            setLoading(true)
-            await auth.signInWithEmailAndPassword(emailRef?.current?.value!, passwordRef?.current?.value!)
-        }catch{
-            setError("failed to log in")
-        }
-        setLoading(false)
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+    try {
+      await auth.signInWithEmailAndPassword(
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (error) {
+      setError(error.message);
     }
-    
-return(
+    setLoading(false);
+  }
+
+  return (
     <Card>
-        <CardContent>
-            <form>
-            <FormControl>
-                {error}
+      <CardContent>
+        <form>
+          <FormControl>
+            {error}
             <InputLabel htmlFor="email">Email address</InputLabel>
-            <OutlinedInput  inputRef={emailRef} id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
-            <FormControl>
+            <OutlinedInput
+              inputRef={emailRef}
+              id="my-input"
+              aria-describedby="my-helper-text"
+            />
+          </FormControl>
+          <FormControl>
             <InputLabel htmlFor="email">password</InputLabel>
-            <OutlinedInput  inputRef={passwordRef} id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
-            </form>        
-        </CardContent>
-        <CardActions>
-            <Button disabled={loading} onClick={(event) => handleSubmit(event)} type="submit" variant="outlined" >
-                Sign up
-            </Button>
-        </CardActions>
+            <OutlinedInput
+              inputRef={passwordRef}
+              id="my-input"
+              aria-describedby="my-helper-text"
+            />
+          </FormControl>
+        </form>
+      </CardContent>
+      <CardActions>
+        <Button
+          disabled={loading}
+          onClick={(event) => handleSubmit(event)}
+          type="submit"
+          variant="outlined"
+        >
+          log in
+        </Button>
+        current user is: {currentUser?.email}
+      </CardActions>
     </Card>
-)
-}
+  );
+};
 
 export default Login;
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,76 +1,99 @@
-import React, {useEffect, useRef,useState} from "react";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel'
-import {
-    useSetRecoilState,
-  } from 'recoil';
-  import { auth } from "../firebase";
-  import {currentUserState} from "../stateManagement/userAuth"
+import React, { useEffect, useRef, useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import { useSetRecoilState } from "recoil";
+import { auth } from "../firebase";
+import firebase from "firebase/app";
 
+import { currentUserState } from "../stateManagement/userAuth";
 
-const SignUp = () =>{
-   const emailRef = useRef<HTMLInputElement>(null);
-   const passwordRef = useRef<HTMLInputElement>(null);
-   const passwordConfirmRef = useRef<HTMLInputElement>(null);
+const SignUp = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordConfirmRef = useRef<HTMLInputElement>(null);
 
-   const setCurrentUser = useSetRecoilState(currentUserState)
-   const [loading, setLoading] = useState(false)
-   const [error, setError] = useState<string>("")
+  const setCurrentUser = useSetRecoilState(currentUserState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>("");
 
-   useEffect(() =>{
-       const unsubscribe = auth.onAuthStateChanged( ( user :  any ) =>{
-           setCurrentUser(user.toJSON())
-           setLoading(false)
-   })
-   return unsubscribe
-    },[])
+  useEffect(() => {
+    auth.onAuthStateChanged((user: any) => {
+      if (user != null) {
+        setCurrentUser(user.toJSON());
+      } else {
+        setCurrentUser(null);
+      }
+      setLoading(false);
+    });
+  }, []);
 
-    async function handleSubmit(e:any){
-        e.preventDefault()
-        if(passwordRef?.current?.value! != passwordConfirmRef?.current?.value!){
-            return setError("Passwords do not match")
-        }
-        try {
-            setError('')
-            setLoading(true)
-            await auth.createUserWithEmailAndPassword(emailRef?.current?.value!, passwordRef?.current?.value!)
-        }catch{
-            setError("Failed to create and account")
-        }
-        setLoading(false)
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+
+    if (passwordRef.current!.value != passwordConfirmRef.current!.value) {
+      return setError("Passwords do not match");
     }
-    
-return(
+    try {
+      setError("");
+      setLoading(true);
+      await auth.createUserWithEmailAndPassword(
+        emailRef?.current?.value!,
+        passwordRef?.current?.value!
+      );
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
+  }
+
+  return (
     <Card>
-        <CardContent>
-            <form>
-            <FormControl>
-                {error}
+      <CardContent>
+        <form>
+          <FormControl>
+            {error}
             <InputLabel htmlFor="email">Email address</InputLabel>
-            <OutlinedInput  inputRef={emailRef} id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
-            <FormControl>
+            <OutlinedInput
+              inputRef={emailRef}
+              id="my-input"
+              aria-describedby="my-helper-text"
+            />
+          </FormControl>
+          <FormControl>
             <InputLabel htmlFor="email">password</InputLabel>
-            <OutlinedInput  inputRef={passwordRef} id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
-            <FormControl>
+            <OutlinedInput
+              inputRef={passwordRef}
+              id="my-input"
+              aria-describedby="my-helper-text"
+            />
+          </FormControl>
+          <FormControl>
             <InputLabel htmlFor="email">password confirm</InputLabel>
-            <OutlinedInput  inputRef={passwordConfirmRef} id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
-            </form>        
-        </CardContent>
-        <CardActions>
-            <Button disabled={loading} onClick={(event) => handleSubmit(event)} type="submit" variant="outlined" >
-                Sign up
-            </Button>
-        </CardActions>
+            <OutlinedInput
+              inputRef={passwordConfirmRef}
+              id="my-input"
+              aria-describedby="my-helper-text"
+            />
+          </FormControl>
+        </form>
+      </CardContent>
+      <CardActions>
+        <Button
+          disabled={loading}
+          onClick={(event) => handleSubmit(event)}
+          type="submit"
+          variant="outlined"
+        >
+          Sign up
+        </Button>
+      </CardActions>
     </Card>
-)
-}
+  );
+};
 
 export default SignUp;
