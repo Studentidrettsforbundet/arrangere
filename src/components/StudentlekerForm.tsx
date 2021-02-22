aa5d5152d301461d99aa4a93520cf46efa3e504a
Karen Dahl Aarhus <44139426+karendahlaarhus@users.noreply.github.com>
2021-02-22 14:15:59 +0100

    Create StudentlekerForm.tsx

---
+++ src/components/StudentlekerForm.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+export default function StudentlekerForm() {
+  console.log("hello");
+  return (
+    <Typography gutterBottom variant="h5" component="h2">
+      Studentleker form
+    </Typography>
+  );
+}
