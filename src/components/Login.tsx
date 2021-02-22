3e68ac77e2b23a9f93410cf7200d46569c7a32b6
Sigrun N <sigrunn@dhcp-10-22-11-221.wlan.ntnu.no>
2021-02-22 13:24:35 +0100

    #3 error validation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,118 +1,82 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
-
-import TextField from "@material-ui/core/TextField";
-
-import { makeStyles } from "@material-ui/core/styles";
-
-import { useRecoilState } from "recoil";
-import { auth } from "../firebase";
-import { currentUserState } from "../stateManagement/userAuth";
+import FilledInput from "@material-ui/core/FilledInput";
+import { Container, Typography } from "@material-ui/core";
+import logo from "../assets/logo-sort.png";
 
 const useStyles = makeStyles({
-  errorText: {
-    color: "red",
+  container: {
+    margin: "0 auto",
+    marginTop: "50px",
+  },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#E8E8E8",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  formfield: {
+    margin: "15px",
+  },
+  image: {
+    padding: "50px",
+    width: "40%",
+  },
+  button: {
+    width: "150px",
+  },
+  text: {
+    margin: "10px",
   },
 });
 
-const Login = () => {
+const LogIn = () => {
   const classes = useStyles();
 
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-
-  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
-  const [loading, setLoading] = useState(false);
-  const [errorText, setErrorText] = useState<string>("");
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [passError, setPassError] = useState<boolean>(false);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user: any) => {
-      if (user != null) {
-        setCurrentUser(user.toJSON());
-      } else {
-        setCurrentUser(null);
-      }
-      setLoading(false);
-    });
-    return unsubscribe;
-  }, []);
-
-  async function handleSubmit(e: any) {
-    e.preventDefault();
-    setErrorText("");
-    setPassError(false);
-    setEmailError(false);
-    setLoading(true);
-
-    await auth
-      .signInWithEmailAndPassword(
-        emailRef.current!.value,
-        passwordRef.current!.value
-      )
-      .catch(function (error) {
-        let code = error.code;
-        if (code == "auth/user-not-found") {
-          setErrorText("Det finnes ingen bruker med denne adressen");
-          return setEmailError(true);
-        } else if (code == "auth/wrong-password") {
-          setErrorText("Feil passord");
-          return setPassError(true);
-        } else if (code == "auth/invalid-email") {
-          setErrorText("Ugyldig epostadresse");
-          return setEmailError(true);
-        } else {
-          setErrorText("Kunne ikke logge inn");
-        }
-      });
-
-    setLoading(false);
-  }
-
   return (
-    <Card>
-      <CardContent>
-        <form>
-          <FormControl>
-            <TextField
-              required
-              label="E-post"
-              inputRef={emailRef}
-              variant="filled"
-              error={emailError}
-            />
-          </FormControl>
-          <FormControl>
-            <TextField
-              required
-              label="Passord"
-              error={passError}
-              inputRef={passwordRef}
-              variant="filled"
-              type="password"
-            />
-          </FormControl>
-        </form>
-        <p className={classes.errorText}> {errorText}</p>
-      </CardContent>
-      <CardActions>
-        <Button
-          disabled={loading}
-          onClick={(event) => handleSubmit(event)}
-          type="submit"
-          variant="outlined"
-        >
-          log in
-        </Button>
-        current user is: {currentUser?.email}
-      </CardActions>
-    </Card>
+    <Container className={classes.container}>
+      <Card className={classes.root}>
+        <img className={classes.image} src={logo} alt="logo" />
+
+        <CardContent className={classes.content}>
+          <Typography variant="h6">Logg inn</Typography>
+          <form className={classes.form}>
+            <FormControl className={classes.formfield}>
+              <Typography variant="body2">Email</Typography>
+              <FilledInput id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+            <FormControl className={classes.formfield}>
+              <Typography variant="body2">Passord</Typography>
+              <FilledInput id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+          </form>
+          <CardActions>
+            <Button type="submit" variant="outlined" className={classes.button}>
+              Registrer
+            </Button>
+          </CardActions>
+        </CardContent>
+
+        <Typography variant="body1" className={classes.text}>
+          Har du ikke konto? <b>Registrer deg her</b>
+        </Typography>
+      </Card>
+    </Container>
   );
 };
 
-export default Login;
+export default LogIn;
