e6df86581c51e869e081fadc170901faad9a41bb
karendaa <karendaa@stud.ntnu.no>
2021-02-22 11:36:15 +0100

    #3 fix routing between login and signup

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,116 +1,81 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import InputLabel from "@material-ui/core/InputLabel";
-import { Link, Typography } from "@material-ui/core";
-import { useRecoilState } from "recoil";
-import { auth } from "../firebase";
-import { currentUserState } from "../stateManagement/userAuth";
-import { Link as RouterLink } from "react-router-dom";
+import FilledInput from "@material-ui/core/FilledInput";
+import { Container, Typography } from "@material-ui/core";
+import logo from "../assets/logo-sort.png";
+
+const useStyles = makeStyles({
+  container: {
+    margin: "0 auto",
+    marginTop: "50px",
+  },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#E8E8E8",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  formfield: {
+    margin: "15px",
+  },
+  image: {
+    padding: "50px",
+    width: "40%",
+  },
+  button: {
+    width: "150px",
+  },
+  text: {
+    margin: "10px",
+  },
+});
 
 const LogIn = () => {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-
-  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string>("");
-
-  /*
-  const login = (email:string, password:string) => {
-      if(email == null || password == null){
-        auth.signInWithEmailAndPassword("", "")
-
-      }
-
-  }
-  */
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user: any) => {
-      if (user != null) {
-        setCurrentUser(user.toJSON());
-      } else {
-        setCurrentUser(null);
-      }
-      setLoading(false);
-    });
-    return unsubscribe;
-  }, []);
-
-  async function handleSubmit(e: any) {
-    e.preventDefault();
-    setError("");
-    setLoading(true);
-
-    await auth
-      .signInWithEmailAndPassword(
-        emailRef.current!.value,
-        passwordRef.current!.value
-      )
-      .catch(function (error: any) {
-        let code = error.code;
-        if (code === "auth/user-not-found") {
-          setError("Det finnes ingen bruker med denne adressen");
-        } else if (code === "auth/wrong-password") {
-          setError("Feil passord");
-        } else if (code === "auth/invalid-email") {
-          setError("Ugyldig epostadresse");
-        } else {
-          setError("Kunne ikke logge inn");
-        }
-      });
-
-    setLoading(false);
-  }
+  const classes = useStyles();
 
   return (
-    <Card>
-      <CardContent>
-        <form>
-          <FormControl>
-            {error}
-            <InputLabel htmlFor="email">Email address</InputLabel>
-            <OutlinedInput
-              inputRef={emailRef}
-              id="my-input"
-              aria-describedby="my-helper-text"
-            />
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="email">password</InputLabel>
-            <OutlinedInput
-              inputRef={passwordRef}
-              id="my-input"
-              aria-describedby="my-helper-text"
-            />
-          </FormControl>
-        </form>
-      </CardContent>
-      <CardActions>
-        <Button
-          disabled={loading}
-          onClick={(event) => handleSubmit(event)}
-          type="submit"
-          variant="outlined"
-        >
-          log in
-        </Button>
-        current user is: {currentUser?.email}
-      </CardActions>
-      <Typography variant="body1">
-        Har du ikke konto?{" "}
-        <b>
-          <Link component={RouterLink} to="/signup">
-            Registrer deg her
-          </Link>
-        </b>
-      </Typography>
-    </Card>
+    <Container className={classes.container}>
+      <Card className={classes.root}>
+        <img className={classes.image} src={logo} alt="logo" />
+
+        <CardContent className={classes.content}>
+          <Typography variant="h6">Logg inn</Typography>
+          <form className={classes.form}>
+            <FormControl className={classes.formfield}>
+              <Typography variant="body2">Email</Typography>
+              <FilledInput id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+            <FormControl className={classes.formfield}>
+              <Typography variant="body2">Passord</Typography>
+              <FilledInput id="my-input" aria-describedby="my-helper-text" />
+            </FormControl>
+          </form>
+          <CardActions>
+            <Button type="submit" variant="outlined" className={classes.button}>
+              Registrer
+            </Button>
+          </CardActions>
+        </CardContent>
+
+        <Typography variant="body1" className={classes.text}>
+          Har du ikke konto? <b>Registrer deg her</b>
+        </Typography>
+      </Card>
+    </Container>
   );
 };
 
