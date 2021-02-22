e794ac45764eb6414e04c8b6a5ac718238cf550f
karendaa <karendaa@stud.ntnu.no>
2021-02-22 18:55:08 +0100

    clean up code, ready for merge into main

--- src/components/ChooseApplication.tsx
+++ src/components/ChooseApplication.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+
+import Student_NM_logo from "./../images/student_NM.png";
+import Studentleker_logo from "./../images/studentleker-1.png";
+import Student_Cup_logo from "./../images/studentcup-1.png";
+
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Student_NM_logo from "./../images/student_NM.png";
-import Studentleker_logo from "./../images/studentleker-1.png";
-import Student_Cup_logo from "./../images/studentcup-1.png";
 import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
 import StudentNMForm from "./StudentNMForm";
 import StudentlekerForm from "./StudentlekerForm";
 import StudentCupForm from "./StudentCupForm";
