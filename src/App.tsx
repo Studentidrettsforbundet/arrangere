8ae1b0509a4c95d4888047397e65d004cdede5e8
johannasofieslinde <johannaslinde@icloud.com>
2021-02-23 10:41:50 +0100

    #21 Add NSI color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Text from "./Text";
+import Buttons from "./Buttons";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const studentidrettTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#8b8b8b",
+      main: "#5e5e5e",
+      dark: "#000000",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#66dfff",
+      main: "#00adee",
+      dark: "#007ebb",
+      contrastText: "#000",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <p>Her skal det komme en søknadsside</p>
-    </div>
+    <ThemeProvider theme={studentidrettTheme}>
+      <div className="App">
+        <p>Her skal det komme en søknadsside</p>
+        <Text></Text>
+        <Buttons></Buttons>
+      </div>
+    </ThemeProvider>
   );
 }
 
