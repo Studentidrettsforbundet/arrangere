dae92c91c83bc1d979bae3bd3aae924b7183d1e0
Siw Døvle <siwd@dhcp-10-22-14-246.wlan.ntnu.no>
2021-02-25 10:56:40 +0100

    Convert firestore data to JSON string

--- src/components/NmForm.tsx
+++ src/components/NmForm.tsx
@@ -1,51 +1,13 @@
 import React from "react";
 import { useState } from "react";
+import { useRecoilState } from "recoil";
 import { firestore } from "../firebase";
+import { NMdataState } from "../stateManagement/firestoreData";
 
 const NmForm = () => {
-  var applicationTemplate = firestore.collection("Søknadsmal");
+  //var applicationTemplate = firestore.collection("Søknadsmal").doc("NM");
 
-  const [data, setData] = useState();
-
-  /*   applicationTemplate
-    .where("type", "==", "shortText")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const temp = [];
-        console.log("Docuemnt data:", doc.data());
-        temp.push(doc.data());
-
-        console.log(temp);
-      });
-    })
-    .catch((error) => {
-      console.log("Error getting docuemnt;", error);
-    }); */
-
-  /*  async function getInputFields() {
-    await firestore
-      .collection("Søknadsmal")
-      .get()
-      .then((querySnapshot) => {
-        const input: any[] | PromiseLike<any[]> = [];
-        querySnapshot.forEach((doc) => {
-          input.push({
-            ...doc.data(),
-            id: doc.id,
-            ref: doc.ref,
-          });
-        });
-        return input;
-      });
-  }
-
-  getInputFields().then((inputArray) => {
-    console.log(inputArray);
-    inputArray.map((user: { displayName: any; }) => {
-      console.log("name: ", user.displayName);
-    });
-  }); */
+  const [data, setData] = useRecoilState(NMdataState);
 
   firestore
     .collection("Søknadsmal")
@@ -54,7 +16,8 @@ const NmForm = () => {
       const data = querySnapshot.forEach(function (doc) {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data());
-        //setData(data);
+        console.log(JSON.stringify(doc.data()));
+        setData(JSON.stringify(doc.data()));
       });
     });
 
@@ -68,6 +31,8 @@ const NmForm = () => {
       <h1>Her skal det forhåpentligvis rendres noe fra firestore:</h1>
       <form onSubmit={onSubmit}>
         <p>todo...</p>
+        {/*         data.map((info) => (
+          )); */}
         {data}
       </form>
     </div>
