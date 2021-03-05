a2daa412e3ae96ec4c10355212840ffca2ffba4f
JulieHM <julieholtemotland@hotmail.com>
2021-03-05 14:08:54 +0100

    #57 touch up

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -2,7 +2,6 @@ import { Typography } from "@material-ui/core";
 
 export const ApplicationForm = (props: any) => {
   let title = props.location.state.title;
-
   return (
     <>
       <Typography gutterBottom variant="h5" component="h2">
