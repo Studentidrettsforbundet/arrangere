c48693f87a72c48f385d6d957b7475c8bc092429
emilielr <43408175+emilielr@users.noreply.github.com>
2021-03-09 10:30:17 +0100

    #69 Add save button to home-page for practise

---
+++ src/components/FirebaseStorage.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+const FirebaseStorage = () => {
+  return (
+    <div>
+      <Button>Lagre dokument til Firestore</Button>
+    </div>
+  );
+};
+
+export default FirebaseStorage;
