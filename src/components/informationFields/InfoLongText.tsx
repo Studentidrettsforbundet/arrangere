23718c73f85622f560e2f0137427f6b6fe0d0ddb
johannasofieslinde <johannaslinde@icloud.com>
2021-03-11 11:25:37 +0100

    #66 Fix imports

--- src/components/informationFields/InfoLongText.tsx
+++ src/components/informationFields/InfoLongText.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
-import { TextField, Typography } from "@material-ui/core";
-import React from "react";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export type InfoLongTextProps = {
@@ -18,10 +19,6 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     margin: "0.5%",
   },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
-  },
 }));
 
 export const InfoLongText: FC<InfoLongTextProps> = ({ desc, title }) => {
@@ -34,7 +31,7 @@ export const InfoLongText: FC<InfoLongTextProps> = ({ desc, title }) => {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography className={classes.heading}>{title}</Typography>
+          <Typography>{title}</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>{desc}</Typography>
