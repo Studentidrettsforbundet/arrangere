0ed7fcc42c392487cf776b390786f82d5eecd1a0
johannasofieslinde <johannaslinde@icloud.com>
2021-03-11 11:20:20 +0100

    #66 Add expandable fields (accordion cards from Material-UI) for displaying information

--- src/components/informationFields/InfoLongText.tsx
+++ src/components/informationFields/InfoLongText.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react";
 import { TextField, Typography } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export type InfoLongTextProps = {
   desc: string;
@@ -7,11 +13,33 @@ export type InfoLongTextProps = {
   title: string;
 };
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    margin: "0.5%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
 export const InfoLongText: FC<InfoLongTextProps> = ({ desc, title }) => {
+  const classes = useStyles();
   return (
-    <div className="infoLongTextContainer">
-      <h1>{title}</h1>
-      <Typography>{desc}</Typography>
+    <div className={classes.root}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{desc}</Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 };
