73a368db69dd47be32d1742291df0c44c78bebe1
siwdovle <43407205+siwdovle@users.noreply.github.com>
2021-03-11 17:25:46 +0100

    Update Home.tsx
    Moved Box import from @material-ui/core/Box to @material-ui/core

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
-import { Typography } from "@material-ui/core";
-import Box from "@material-ui/core/Box";
+import { Typography, Box } from "@material-ui/core";
 import React from "react";
 import FirebaseStorage from "./FirebaseStorage";
 
