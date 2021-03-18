e8ff8d42c01abb7f9790fc53e55a8ca0d2551fd6
johannasofieslinde <johannaslinde@icloud.com>
2021-03-18 10:00:53 +0100

    #110 File upload to Storage (not finished)

--- src/components/inputFields/FileUpload.tsx
+++ src/components/inputFields/FileUpload.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
 import { Typography, Box } from "@material-ui/core";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  DefaultValue,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 import {
   attributesState,
   selectedAttributeIdState,
 } from "../../stateManagement/attributesState";
+import firebase from "firebase";
 
 type FileUploadProps = {
   desc: string;
@@ -17,22 +23,32 @@ const FileUpload: FC<FileUploadProps> = ({ desc, id }) => {
 
   const selectedID = useRecoilValue(selectedAttributeIdState);
 
+  const saveFile = async (event: any) => {
+    const file = event.target.files;
+    const fileLocation = firebase.storage().ref("files").child(selectedID);
+    fileLocation.put(file);
+    console.log("file attribute saved: ", file);
+  };
+
+  const handleChange = (event: any) => {
+    saveFile(event);
+    setAttribute({
+      ...attribute,
+      value: event.target.value,
+      id: selectedID,
+    });
+  };
+
   return (
     <Box py={2}>
       <Typography>{desc}</Typography>
       <input
-        accept="image/*"
+        accept="pdf"
         id="contained-button-file"
         multiple
         type="file"
         onFocus={() => setSelectedAttribute(id)}
-        onChange={(event) =>
-          setAttribute({
-            ...attribute,
-            value: event.target.value,
-            id: selectedID,
-          })
-        }
+        onChange={(event) => handleChange(event)}
       />
     </Box>
   );
