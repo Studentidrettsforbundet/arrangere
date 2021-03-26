4a6a864fb18a0936cf88c0eb26e4257ac5ea6987
Siw Døvle <siwd@Siws-MacBook-Pro.local>
2021-03-26 08:43:41 +0100

    #83 Set firebaseConfig explicity

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC3VrrCu2PeD4nQMDSCHsaZQ1rhUE7U03Y",
+  authDomain: "arrangere-a8fca.firebaseapp.com",
+  databaseURL: "https://arrangere-a8fca-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "arrangere-a8fca",
+  storageBucket: "arrangere-a8fca.appspot.com",
+  messagingSenderId: "453649241479",
+  appId: "1:453649241479:web:6450f5d0d16186e1f2af22",
 };
 
 // Initialize Firebase
