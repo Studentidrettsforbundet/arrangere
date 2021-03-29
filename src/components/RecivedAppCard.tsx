649777da8fd817c5867ab3e33919b93a312dea99
JulieHM <julieholtemotland@hotmail.com>
2021-03-29 12:43:34 +0200

    #116 #117 add card for each application for admin view, onclick on card to console.log  application content

--- src/components/RecivedAppCard.tsx
+++ src/components/RecivedAppCard.tsx
@@ -27,29 +27,58 @@ export const RecivedAppCard = (props: Props) => {
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          console.log(`${doc.id}`);
+          //console.log(`${doc.id}`);
           setApplicationList((applicationList: any) => [
             ...applicationList,
             `${doc.id}`,
           ]);
         });
       });
+    return applicationList;
   }
 
-  /** Må skrive kode for å hente ut brukernavn til bruker som har sendt inn søknaden */
-  // function getUsername() {
+  /** Må skrive kode for å hente ut brukernavn til bruker som har sendt inn søknaden, noe lignende
+   * det under
+   */
+  // function getUsername(applicationList: any) {
   //   db.collection("user")
   //     .get()
   //     .then((querySnapshot) => {
   //       querySnapshot.forEach((doc) => {
-  //         console.log(`${doc.id}`);
-  //         setApplicationList((applicationList: any) => [
-  //           ...applicationList,
-  //           `${doc.id}`,
-  //         ]);
+  //         //console.log(doc.data().applications);
+  //         if (doc.data() != null) {
+  //           console.log(doc.data().applications);
+
+  //             if (doc.data().applications.contains(application)) {
+  //               console.log(doc.data().email);
+  //             } else {
+  //               console.log("no match");
+  //             }
+  //           });
+  //         } else {
+  //           console.log("fail");
+  //         }
   //       });
   //     });
   // }
+
+  function showApplication(item: string) {
+    console.log(item);
+    db.collection(props.collectionName)
+      .doc(item)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }
 
   return (
     <div style={{ display: "flex" }}>
@@ -60,6 +89,7 @@ export const RecivedAppCard = (props: Props) => {
               className={classes.title}
               color="textSecondary"
               gutterBottom
+              key="i"
             >
               {item}
             </Typography>
@@ -72,7 +102,11 @@ export const RecivedAppCard = (props: Props) => {
             </Typography>
           </CardContent>
           <CardActions>
-            <Button variant="outlined" size="small">
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => showApplication(item)}
+            >
               Vis søknad
             </Button>
           </CardActions>
