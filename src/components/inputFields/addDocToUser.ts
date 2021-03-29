c3b14f8326bfd7a1ac35a6b64f6b9c5d50adcc7d
emilielr <43408175+emilielr@users.noreply.github.com>
2021-03-29 15:48:12 +0200

    #97 Document is connected to user

---
+++ src/components/inputFields/addDocToUser.ts
@@ -0,0 +1,39 @@
+import { firestore } from "../../firebase";
+
+async function getNumberOfApplications(userID: string) {
+  let counter: number = 1;
+  const doc = await firestore.collection("user").doc(userID).get();
+
+  const docData: any = doc.data();
+  for (const application in docData.applications) {
+    counter++;
+  }
+  return counter;
+}
+
+export const addDocToUser = async (userID: string, docID: string) => {
+  let applicationNr: number = 0;
+
+  await getNumberOfApplications(userID).then(
+    (counter) => (applicationNr = counter)
+  );
+
+  if (applicationNr == 0) {
+    firestore
+      .collection("user")
+      .doc(userID)
+      .set({ applications: { application1: docID } })
+      .then(() => console.log("Document added to user!"))
+      .catch((error) => console.log(error));
+  } else {
+    let data: any = {};
+    data[`applications.application${applicationNr}`] = docID;
+
+    firestore
+      .collection("user")
+      .doc(userID)
+      .update(data, { merge: true })
+      .then(() => console.log("Document added to user!"))
+      .catch((error) => console.log(error));
+  }
+};
