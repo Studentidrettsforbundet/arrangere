5912cff38f51f8010475ac59262af45dbbecf59e
siwdovle <siwdovle@users.noreply.github.com>
2021-03-29 14:52:40 +0000

    Apply formatting changes

--- src/firebase.js
+++ src/firebase.js
@@ -21,10 +21,10 @@ export const auth = app.auth();
 
 //eslint-disable-next-line no-restricted-globals
 if (location.hostname === "localhost") {
-   firestore.useEmulator("localhost", 8080);
-   firebase
-     .auth()
-     .useEmulator("http://localhost:9099/", { disableWarnings: true });
- }
+  firestore.useEmulator("localhost", 8080);
+  firebase
+    .auth()
+    .useEmulator("http://localhost:9099/", { disableWarnings: true });
+}
 
 export default app;
