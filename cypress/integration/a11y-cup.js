596791d89d16d50ef9fedce9d83e2c7b6c704872
sigrunnu <sigrunn@dhcp-10-22-8-121.wlan.ntnu.no>
2021-04-12 08:24:57 +0200

    #145 - updated test

--- cypress/integration/a11y-cup.js
+++ cypress/integration/a11y-cup.js
@@ -1,17 +1,17 @@
 beforeEach(() => {
   cy.visit("/studentcup");
+  cy.wait(1000);
   cy.injectAxe();
-
-  describe("Accessibility", () => {
-    it("should be accessible", () => {
-      cy.get(".chapterButtons")
-        .find("button")
-        .each(($button) => {
-          cy.wrap($button).click();
-          cy.wait(1000).then(() => {
-            cy.checkA11y();
-          });
+});
+describe("Accessibility", () => {
+  it("should be accessible", () => {
+    cy.get(".chapterButtons")
+      .find("button")
+      .each(($button) => {
+        cy.wrap($button).click();
+        cy.wait(1000).then(() => {
+          cy.checkA11y();
         });
-    });
+      });
   });
 });
