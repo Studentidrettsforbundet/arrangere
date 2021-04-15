8e6a252df6dad118bb5bf9db9f07ca184e5137e6
Siw Døvle <siwd@dhcp-10-22-13-195.wlan.ntnu.no>
2021-04-15 09:05:21 +0200

    cypress.yml change

--- src/firebase.js
+++ src/firebase.js
@@ -19,7 +19,7 @@ const app = firebase.initializeApp(firebaseConfig);
 export const firestore = firebase.firestore();
 export const auth = app.auth();
 
-if (location.hostname === "localhost") {
+if (window.location.hostname === "localhost") {
   firestore.useEmulator("localhost", 8080);
   firebase
     .auth()
