f3b36d44cd1eddf8efa5698da7c42175d1e5b2b4
Siw Døvle <siwd@Siws-MacBook-Pro.local>
2021-04-17 09:32:24 +0200

    Delete document after test, add test for saveInputFields and getInputValue

--- src/addDoc.test.ts
+++ src/addDoc.test.ts
@@ -3,37 +3,12 @@
  */
 
 //import firebase from "@firebase/rules-unit-testing";
-import {addFieldInputObject} from "./components/inputFields/saveInputFields"
+import {addFieldInputObject, saveInput} from "./components/inputFields/saveInputFields"
 import {getNumberOfApplications, addDocToUser} from "./components/inputFields/addDocToUser"
 import {copyDoc} from "./components/inputFields/copyDoc"
-import firebase, { firestore } from "./firebase";
-/* import { auth } from "./firebase";
-import firebase from "firebase";
-
-beforeEach(() => {
-    var db = firebase.firestore();
-    auth.createUserWithEmailAndPassword(
-        "siw@gmail.com",
-        "siw1234"
-      )
-      .then((cred) => {
-        if (cred.user != null) {
-          return db.collection("user").doc(cred.user.uid).set({
-            organization: "",
-            role: "",
-            email: "siw@gmail.com",
-            applications: {},
-          });
-        }
-      })
-      .catch((err: any) => {
-        console.log("Something went wrong")
-      });
-}); */
-
-afterEach(() => {
-    
-  });
+import { firestore } from "./firebase";
+import firebase from "firebase"
+import { getInputValue } from "./components/inputFields/getInputValue";
 
 describe("", () => {
 it("test if addFieldInputObject sets fields", () => {
@@ -45,14 +20,20 @@ it("test if addFieldInputObject sets fields", () => {
 
 it("Test if doc is added to user", async () => {
 
-    await addDocToUser("QY1khY1DrpNUD20E1QcXKTypeBte", "newDocIDI7OUcA12yWZ5", "snm")
-    const counter = await getNumberOfApplications("QY1khY1DrpNUD20E1QcXKTypeBte");
-    expect(counter).toEqual(2);
+    await addDocToUser("y0Z97CZIC0NPEz2QbUl3BoXzvPZz", "newDocIDI7OUcA12yWZ5", "snm")
+    const counter = await getNumberOfApplications("y0Z97CZIC0NPEz2QbUl3BoXzvPZz");
+    expect(counter).toEqual(3);
+
+    await firestore.collection("user").doc("y0Z97CZIC0NPEz2QbUl3BoXzvPZz").set({"applications" : { ["application2"]: firebase.firestore.FieldValue.delete()}}, { merge: true }).then(() => {
+      console.log("Document successfully deleted!");
+  }).catch((error) => {
+      console.error("Error removing document: ", error);
+  });
 });
 
 it("Get number of applications", async () => {
     
-    const counter = await getNumberOfApplications("QY1khY1DrpNUD20E1QcXKTypeBte");
+    const counter = await getNumberOfApplications("y0Z97CZIC0NPEz2QbUl3BoXzvPZz");
     expect(counter).toEqual(2);
 });
 });
@@ -61,3 +42,12 @@ it("Copy doc from template to application collection", async () => {
     const newDocID = await copyDoc("snm");
     const data =  expect((await firestore.collection("snmApplications").doc(newDocID).get()).exists).toBeTruthy()
 })
+
+it("Test if input value gets saved and retrived", async () => {
+  const object = {"general-3": "Fotball", chapterName: "general"}
+  const docRef = firestore.collection("snmApplications").doc("7uIx8AWpva80rOH7Vyvl");
+  await saveInput(docRef, object)
+  const value = await getInputValue(docRef, "general", "general-3")
+  expect(value).toEqual("Fotball")
+})
+
