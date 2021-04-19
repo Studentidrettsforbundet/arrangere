9c2c90320431f86aa1823accf89cb5b9c6d9e85c
siwdovle <siwdovle@users.noreply.github.com>
2021-04-19 12:04:16 +0000

    Apply formatting changes

--- src/test/saveInput.test.ts
+++ src/test/saveInput.test.ts
@@ -2,13 +2,15 @@
  * @jest-environment node
  */
 
- import {addFieldInputObject, saveInput} from "../components/inputFields/saveInputFields"
- import { getInputValue } from "../components/inputFields/getInputValue";
- import { firestore } from "../firebase";
- 
- describe("Test", () => {
+import {
+  addFieldInputObject,
+  saveInput,
+} from "../components/inputFields/saveInputFields";
+import { getInputValue } from "../components/inputFields/getInputValue";
+import { firestore } from "../firebase";
 
-     /*  it("test if addFieldInputObject sets fields", () => {
+describe("Test", () => {
+  /*  it("test if addFieldInputObject sets fields", () => {
     const inputFieldObject = addFieldInputObject(
       "Trondheim",
       "general",
@@ -19,11 +21,13 @@
     expect(inputFieldObject.chapterName).toEqual("general");
   }); */
 
-    it("Test if input value gets saved and retrived", async () => {
-        const object = {"general-3": "Fotball", chapterName: "general"}
-        const docRef = firestore.collection("snmApplications").doc("ot8ttU6nGW7mtpltrZGZ");
-        await saveInput(docRef, object)
-        const value = await getInputValue(docRef, "general", "general-3")
-        expect(value).toEqual("Fotball")
-      })
- })
+  it("Test if input value gets saved and retrived", async () => {
+    const object = { "general-3": "Fotball", chapterName: "general" };
+    const docRef = firestore
+      .collection("snmApplications")
+      .doc("ot8ttU6nGW7mtpltrZGZ");
+    await saveInput(docRef, object);
+    const value = await getInputValue(docRef, "general", "general-3");
+    expect(value).toEqual("Fotball");
+  });
+});
