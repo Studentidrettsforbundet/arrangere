18e694eae9989ef830f1fd44c7fffdcd35f786e6
JulieHM <julieholtemotland@hotmail.com>
2021-04-20 12:58:33 +0200

    clean up code

--- src/components/ChooseApplication.tsx
+++ src/components/ChooseApplication.tsx
@@ -47,8 +47,6 @@ export const ChooseApplication = () => {
             });
           }
         }
-        // setSubmittedApplicationIDs(submittedApplicationIDs);
-        // setInProgressApplicationIDs(inProgressApplicationIDs);
       }
     }
     setSubmittedApplicationIDs(submittedApplicationIDs);
