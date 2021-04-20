eca7570f8113e45d0a796dc02e387a235b2e62f5
siwdovle <siwdovle@users.noreply.github.com>
2021-04-20 13:46:26 +0000

    Apply formatting changes

--- src/test/saveInput.test.ts
+++ src/test/saveInput.test.ts
@@ -1,42 +1,42 @@
 /**
-* @jest-environment node
-*/
+ * @jest-environment node
+ */
 import {
-    addFieldInputObject,
-    saveInput,
-    } from "../components/inputFields/saveInputFields";
-    import { getInputValue } from "../components/inputFields/getInputValue";
-    import { firestore } from "../firebase";
-    describe("Test", () => {
-    it("test if addFieldInputObject sets fields", () => {
+  addFieldInputObject,
+  saveInput,
+} from "../components/inputFields/saveInputFields";
+import { getInputValue } from "../components/inputFields/getInputValue";
+import { firestore } from "../firebase";
+describe("Test", () => {
+  it("test if addFieldInputObject sets fields", () => {
     const inputFieldObject = addFieldInputObject(
-    "Trondheim",
-    "general",
-    {},
-    "general-3"
+      "Trondheim",
+      "general",
+      {},
+      "general-3"
     );
     expect(inputFieldObject).toBeDefined();
     expect((<any>inputFieldObject).chapterName).toEqual("general");
-    });
-    it("Test if input value gets saved and retrived", async () => {
+  });
+  it("Test if input value gets saved and retrived", async () => {
     const object = { "general-3": "Fotball", chapterName: "general" };
     const docRef = firestore
-    .collection("snmApplications")
-    .doc("ot8ttU6nGW7mtpltrZGZ");
+      .collection("snmApplications")
+      .doc("ot8ttU6nGW7mtpltrZGZ");
     await saveInput(docRef, object);
     const value = await getInputValue(docRef, "general", "general-3");
     expect(value).toEqual("Fotball");
-    });
-    it("Test if input value gets saved and retrived in accordion", async () => {
+  });
+  it("Test if input value gets saved and retrived in accordion", async () => {
     const object = {
-    "activity-1": "Butterfly",
-    "activity-2": "Amatør",
-    "activity-3": "Dame",
-    chapterName: "activities",
+      "activity-1": "Butterfly",
+      "activity-2": "Amatør",
+      "activity-3": "Dame",
+      chapterName: "activities",
     };
     const docRef = firestore
-    .collection("snmApplications")
-    .doc("ot8ttU6nGW7mtpltrZGZ");
+      .collection("snmApplications")
+      .doc("ot8ttU6nGW7mtpltrZGZ");
     await saveInput(docRef, object);
     const value1 = await getInputValue(docRef, "activities", "activity-1");
     expect(value1).toEqual("Butterfly");
@@ -44,5 +44,5 @@ import {
     expect(value2).toEqual("Amatør");
     const value3 = await getInputValue(docRef, "activities", "activity-3");
     expect(value3).toEqual("Dame");
-    });
-    });
+  });
+});
--- src/test/setStatusToSubmittes.test.ts
+++ src/test/setStatusToSubmittes.test.ts
@@ -1,57 +1,55 @@
 /**
-* @jest-environment node
-*/
+ * @jest-environment node
+ */
 
-import {
-    saveInput,
-    } from "../components/inputFields/saveInputFields";
-    import { getInputValue } from "../components/inputFields/getInputValue";
-    import { firestore } from "../firebase";
-    import { setStatusToSubmitted } from "../components/inputFields/setStatusToSubmitted";
-    describe("Test", () => {
-    it("Test if status field gets updated to submitted on doc", async () => {
+import { saveInput } from "../components/inputFields/saveInputFields";
+import { getInputValue } from "../components/inputFields/getInputValue";
+import { firestore } from "../firebase";
+import { setStatusToSubmitted } from "../components/inputFields/setStatusToSubmitted";
+describe("Test", () => {
+  it("Test if status field gets updated to submitted on doc", async () => {
     const docRef = firestore
-    .collection("snmApplications")
-    .doc("ot8ttU6nGW7mtpltrZGZ");
+      .collection("snmApplications")
+      .doc("ot8ttU6nGW7mtpltrZGZ");
     await setStatusToSubmitted(
-    docRef,
-    "SqHIu7oPUeaQKU19ev2UFpjXMsv2",
-    "ot8ttU6nGW7mtpltrZGZ"
+      docRef,
+      "SqHIu7oPUeaQKU19ev2UFpjXMsv2",
+      "ot8ttU6nGW7mtpltrZGZ"
     );
     let fieldPath = `status`;
     let value = await firestore
-    .collection("snmApplications")
-    .doc("ot8ttU6nGW7mtpltrZGZ")
-    .get()
-    .then((res: any) => {
-    return res.get(fieldPath);
-    })
-    .catch((error: any) => {
-    console.log("Error in retrieving value:", error);
-    });
+      .collection("snmApplications")
+      .doc("ot8ttU6nGW7mtpltrZGZ")
+      .get()
+      .then((res: any) => {
+        return res.get(fieldPath);
+      })
+      .catch((error: any) => {
+        console.log("Error in retrieving value:", error);
+      });
     expect(value).toEqual("submitted");
-    });
-    it("Test if status field gets updated to submitted on user", async () => {
+  });
+  it("Test if status field gets updated to submitted on user", async () => {
     const docRef = firestore
-    .collection("snmApplications")
-    .doc("ot8ttU6nGW7mtpltrZGZ");
+      .collection("snmApplications")
+      .doc("ot8ttU6nGW7mtpltrZGZ");
     await setStatusToSubmitted(
-    docRef,
-    "SqHIu7oPUeaQKU19ev2UFpjXMsv2",
-    "ot8ttU6nGW7mtpltrZGZ"
+      docRef,
+      "SqHIu7oPUeaQKU19ev2UFpjXMsv2",
+      "ot8ttU6nGW7mtpltrZGZ"
     );
     let fieldPath = `applications.ot8ttU6nGW7mtpltrZGZ.status`;
     let value = await firestore
-    .collection("user")
-    .doc("SqHIu7oPUeaQKU19ev2UFpjXMsv2")
-    .get()
-    .then((res: any) => {
-    console.log(res.get(fieldPath));
-    return res.get(fieldPath);
-    })
-    .catch((error: any) => {
-    console.log("Error in retrieving value:", error);
-    });
+      .collection("user")
+      .doc("SqHIu7oPUeaQKU19ev2UFpjXMsv2")
+      .get()
+      .then((res: any) => {
+        console.log(res.get(fieldPath));
+        return res.get(fieldPath);
+      })
+      .catch((error: any) => {
+        console.log("Error in retrieving value:", error);
+      });
     expect(value).toEqual("submitted");
-    });
-    });
+  });
+});
