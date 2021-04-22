982dded2206308aa9fd804490cdf83736f9ca1ac
emilielr <43408175+emilielr@users.noreply.github.com>
2021-04-22 15:40:28 +0200

    Remove unused functions in utils

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -3,23 +3,3 @@ import React from "react";
 export function is_numeric(str: string) {
   return /^\d+$/.test(str);
 }
-
-export function splitToAttributeName(str: string) {
-  let attributeName: string = "";
-  str.split("").forEach((character) => {
-    if (!is_numeric(character)) {
-      attributeName += character;
-    }
-  });
-  return attributeName;
-}
-
-export function splitToInputNr(str: string) {
-  let inputNr: string = "";
-  str.split("").forEach((character) => {
-    if (is_numeric(character)) {
-      inputNr += character;
-    }
-  });
-  return inputNr;
-}
